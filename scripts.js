ee9e97b2bcfb94ee0051762f6f07c789477d4ccc
Alexander McKinlay <63086408+AlexEmSiKay@users.noreply.github.com>
2021-09-27 11:43:39 +1000

    added variable functionality

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ var paras
 var parasText = []
 document.addEventListener("DOMContentLoaded", function () {
     paras = document.getElementsByTagName("p");
+    vars = document.getElementsByClassName("variable")
+    console.log(vars)
     for (var i = 0; i < paras.length; i++) {
         parasText.push(paras[i].innerHTML);
     }
@@ -14,7 +16,8 @@ function copyFunction(para) {
     navigator.clipboard.writeText(document.getElementById(para).innerHTML.replace(/\s\s+/g, " ").replace(/<br>/g, "\n"));
 }
 
-function updateParas(q) {
+function updateParas() {
+    q = document.getElementById("quantity").value
     var h = Math.floor(q / 60);
     var m = q % 60
     var d = q / 100
@@ -27,8 +30,20 @@ function updateParas(q) {
     }
     for (var i = 0; i < paras.length; i++) {
         paras[i].innerHTML = parasText[i].replace(/{{}}/g, q).replace(/{{h}}/g, h).replace(/{{m}}/g, m).replace(/{{d}}/g, d).replace(/{{dF}}/g, dFixed).replace(/{{l}}/g, lead);
+        console.log(vars[i].value);
+        if (vars[i].value != "") {
+            var extra = (paras[i].innerHTML + ',<br>' + paras[i].innerHTML).split('<br>');
+            console.log(extra)
+            for (var j = 0; j < extra.length / 2; j++) {
+                extra[j] = '"' + vars[i].value + " = " + extra[j].slice(1);
+            }
+            for (var j = extra.length / 2; j < extra.length; j++) {
+                extra[j] = extra[j].slice(0, ((j == extra.length - 1) ? -1 : -2)) + " = " + vars[i].value + '",';
+            }
+            paras[i].innerHTML += ",<br>" + extra.join('<br>')
+        }
+        // console.log("function called");
     }
-    // console.log("function called");
 }
 
 function update_escape() {
